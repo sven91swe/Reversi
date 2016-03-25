2989a26014dbb78f61e4030c7f3a8c4aa5e308bd
sven91swe <sven_swe@hotmail.com>
2016-03-25 12:54:21 +0100

    Started to add the bot structure.

---
+++ src/gameBot/ExamplePlayer/ExampleBot.java
@@ -0,0 +1,20 @@
+package gameBot.ExamplePlayer;
+
+import gameBot.GameBot;
+
+/**
+ * Created by Sven Eriksson on 2016-03-25.
+ */
+public class ExampleBot extends GameBot {
+    public ExampleBot(){
+        super();
+        this.setCreator("ExampleUser");
+        this.setName("ExampleBot");
+        this.setVersion(-1);
+
+    }
+    @Override
+    public void calculateNextMove(int[][] board, int colour) {
+
+    }
+}
---
+++ src/gameBot/GameBotCreator.java
@@ -0,0 +1,9 @@
+package gameBot;
+
+/**
+ * Created by Sven Eriksson on 2016-03-25.
+ */
+public abstract class GameBotCreator {
+
+    //Implementera singleton för detta objekt. Detta är ansvarigt för att förse spelet med ny instanser av dess bot.
+}
