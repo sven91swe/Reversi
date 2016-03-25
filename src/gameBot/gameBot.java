5b67883649d280a367e70717d4ec9e7dbc11f509
sven91swe <sven_swe@hotmail.com>
2016-03-25 22:15:06 +0100

    Started to add the bot structure.

--- src/gameBot/gameBot.java
+++ src/gameBot/gameBot.java
@@ -4,9 +4,11 @@ package gameBot;
  * Created by Sven Eriksson on 2016-03-25.
  */
 abstract public class GameBot {
-    boolean isRunning = false;
-    int[] nextMove = null;
-
+    private boolean isRunning = false;
+    private int[] nextMove = null;
+    private int version = 0;
+    private String name = "Unknown";
+    private String creator = "Unknown";
 
     /**
      * This method is responsible for doing the calculation for the next move.
@@ -16,7 +18,7 @@ abstract public class GameBot {
      * It is highly recommended that isRunning is set to true and nextMove to null when this method is initialized.
      * Once isRunning is changed to false, this method must be exited within 2 seconds.
      *
-     * @param board
+     * @param board 10x10 2d array. Outer edges are -1, empty areas are 0, 1 and 2 represent game pieces
      */
     public abstract void calculateNextMove(int[][] board, int colour);
 
@@ -26,7 +28,7 @@ abstract public class GameBot {
      * null means pass
      * Otherwise an array with two integers in the range [1,8] is expected. This will indicate the move that you will do.
      */
-    private void setNextMove(int[] nextMove){
+    protected void setNextMove(int[] nextMove){
         this.nextMove = nextMove.clone();
     }
 
@@ -36,5 +38,29 @@ abstract public class GameBot {
 
     public void stopCalculating(){
         this.isRunning = false;
+    }
+
+    public int getVersion(){
+        return this.version;
+    }
+
+    protected void setVersion(int v){
+        this.version = v;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    protected void setName(String n){
+        this.name = n;
+    }
+
+    public String getCreator(){
+        return this.creator;
+    }
+
+    protected void setCreator(String c){
+        this.creator = c;
     }
 }
