09e745b5e2f834b250135951fde7e725b6379620
sven91swe <sven_swe@hotmail.com>
2016-03-26 01:50:26 +0100

    Wrote a class for creating objects from gameBots.

--- src/gameBot/GameBotCreator.java
+++ src/gameBot/GameBotCreator.java
@@ -4,6 +4,35 @@ package gameBot;
  * Created by Sven Eriksson on 2016-03-25.
  */
 public abstract class GameBotCreator {
+    protected GameBot lastGameBot = null;
+
+    private GameBot getLastGameBot(){
+        if(this.lastGameBot == null){
+            lastGameBot = this.getNewGameBot();
+        }
+        return lastGameBot;
+    }
+
+    public GameBot getNewGameBot(){
+        this.lastGameBot = this.createNewGameBot();
+        return this.lastGameBot;
+    }
+
+    protected abstract GameBot createNewGameBot();
+
+
+    public String getName(){
+        return this.getLastGameBot().getName();
+    }
+
+    public String getCreator(){
+        return this.getLastGameBot().getCreator();
+    }
+
+    public int getVersion(){
+        return this.getLastGameBot().getVersion();
+    }
+
 
     //Implementera singleton för detta objekt. Detta är ansvarigt för att förse spelet med ny instanser av dess bot.
 }
