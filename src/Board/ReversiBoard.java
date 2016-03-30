25173c5e50281d3b5042e66ad53356f90a80e322
Sven <svene@student.chalmers.se>
2016-03-30 19:38:11 +0200

    Added a function to calculate current score for each color.

--- src/Board/ReversiBoard.java
+++ src/Board/ReversiBoard.java
@@ -286,4 +286,17 @@ public class ReversiBoard {
         }
         return board;
     }
+
+    //TODO: Add javadoc.
+    public int getScore(int color){
+        int sum = 0;
+        for(int i=1; i<=8; i++){
+            for(int j=1; j<=8; j++){
+                if(this.boardState[i][j]==color){
+                    sum++;
+                }
+            }
+        }
+        return sum;
+    }
 }
