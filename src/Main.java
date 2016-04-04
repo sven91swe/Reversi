f0c2d3d9a0cd2bae8ab85faf80d0e64569a1aefc
sven91swe <sven_swe@hotmail.com>
2016-04-04 14:07:55 +0200

    Removed support class and updated JavaDoc

--- src/Main.java
+++ src/Main.java
@@ -14,19 +14,6 @@ public class Main {
         test5();
     }
 
-    public static void test1(){
-        System.out.println("Board with markers in the middle (board2)");
-        Support.printBoard(Support.newBoard());
-
-
-        GameBotCreator c = new ExampleBotCreator();
-        GameBot g = c.getNewGameBot();
-
-        g.calculateNextMove(null, 1);
-        System.out.print(g.getMove().getX() + "  " + g.getMove().getY() + "\n");
-        System.out.print(c.getCreator() + " - " + c.getName() + " - " + c.getVersion());
-    }
-
     public static void test2(){
         ReversiBoard board = new ReversiBoard();
         board.printBoard();
