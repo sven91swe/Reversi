e2fee4922ffc4343fe75913ed95b7c83a7b5d50c
Mattias Kjelltoft <matkj@student.chalmers.se>
2016-04-11 01:37:22 +0200

    Worked more on the tree search. Also added equals and copy methods to Move class.

--- src/GameBot/MKBot/MKBot.java
+++ src/GameBot/MKBot/MKBot.java
@@ -27,7 +27,7 @@ public class MKBot extends GameBot {
         // Safety measure, set the move to something valid
         ArrayList<Move> list = reversiBoard.allPotentialMoves(color);
         Move move = null;
-        if(list.size() != 0) {
+        if(list.size() > 0) {
             move = list.get(0);
         }else{
             move = new Move(true);
@@ -38,7 +38,39 @@ public class MKBot extends GameBot {
         if(root == null){ // Create the tree if it is not existing
             root = new Node( null, null, 1, reversiBoard);
         }else{ // Prune the tree and keep only the branch of the move just executed
-            // TODO
+            root = root.findChildByMove(allPreviousMoves.get(allPreviousMoves.size()-2)); // Protagonists last move
+            root = root.findChildByMove(allPreviousMoves.get(allPreviousMoves.size()-1)); // Opponents last move
+        }
+        
+        // Start or continue the breadth first search
+        //      Check if latest tier are done
+        //      Spawn next tier or continue on last tier
+        //      Update node values of next tier
+        //      Update branch values 
+        //      Update current move
+        //      Check the time and maybee exit
+        int i=0; // TODO Fix a timer
+        if(root.getNumberOfChildren() > 0){
+            do{
+                    Node.resetTraversedCounter();
+                    if(!(i<3)){ // TODO Fix a timer
+                        break; // TODO return gracefully
+                    }
+            }while(!root.updateLeafNodeValues());
+            root.updateBranchValue();
+            move = root.getBestMove();
+        }
+        while(i<3){ // TODO Fix a timer
+            root.spawnNewChildTier();
+            do{
+                Node.resetTraversedCounter();
+                if(!(i<3)){ // TODO Fix a timer
+                    break; // TODO return gracefully
+                }
+            }while(!root.updateLeafNodeValues());
+            root.updateBranchValue();
+            move = root.getBestMove();
+            i++; // TODO Fix a timer
         }
         
         this.isRunning = false;
